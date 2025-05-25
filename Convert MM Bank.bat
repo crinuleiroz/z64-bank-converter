39162e749103c4b1592309c20313ae3f460ecb66
Crin <usanimere@outlook.com>
2025-05-25 02:08:29 -0700

    Create Convert MM Bank.bat

---
+++ Convert MM Bank.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+:: Check the number of files in the input
+if "%~2"=="" (
+  :: Single file
+  python "Zelda64 Bank Converter.py" "%~1" -g mm
+) else (
+  :: Two files
+  python "Zelda64 Bank Converter.py" "%~1" "%~2" -g mm
+)
+
+endlocal
+pause
