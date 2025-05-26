5e40ec76f259263872f3328171730cc64c2af65a
Crin <usanimere@outlook.com>
2025-05-25 23:41:42 -0700

    Create YAMLSerializer.py

---
+++ utils/YAMLSerializer.py
@@ -0,0 +1,9 @@
+import yaml
+
+class FlowStyleList(list):
+    pass
+
+def represent_flow_style_list(dumper, data):
+    return dumper.represent_sequence('tag:yaml.org,2002:seq', data, flow_style=True)
+
+yaml.add_representer(FlowStyleList, represent_flow_style_list)
